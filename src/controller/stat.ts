9c524220fc21f7331a7e46b74f4d41772624ab6d
Sergei <kalyuzhni.sergei@gmail.com>
2021-06-11 19:11:29 +0300

    Add logging to app

--- src/controller/stat.ts
+++ src/controller/stat.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import { IProbabilityRequest } from '../interface';
+import { log } from '../logs/logger';
 import { StatisticGenerator } from '../service/stat-generator-helper';
 
 const statRouter = express.Router();
 statRouter.get('/', (req: IProbabilityRequest, res) => {
   const { probability, value, iterations } = req.query;
+  log.info('query', { ...req.query });
 
   // TODO Extract logic to reposity if DB appears
   const statGenerator = new StatisticGenerator(probability, value, iterations);
   const statistics = statGenerator.generateProbabilityStat();
 
+  log.info('response', statistics);
   res.send(statistics);
 });
 
---
+++ src/logs/logger.ts
@@ -0,0 +1,20 @@
+import bunyan, { LogLevel } from 'bunyan';
+import path from 'path';
+
+const level = (process.env.NODE_LOGGING_LEVEL || 'info') as LogLevel;
+
+const log = bunyan.createLogger({
+  name: 'logger',
+  streams: [
+    {
+      level,
+      stream: process.stdout,
+    },
+    {
+      level,
+      path: path.resolve(__dirname, './logs.json'),
+    },
+  ],
+});
+
+export { log };
