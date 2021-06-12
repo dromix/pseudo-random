9459bc7e2d467e0c0b75936b1e09d7f1d0492751
Sergei <kalyuzhni.sergei@gmail.com>
2021-06-12 22:50:02 +0300

    Configure logging for Heroku

--- src/service/logger.ts
+++ src/service/logger.ts
@@ -1,5 +1,8 @@
 import bunyan, { LogLevel } from 'bunyan';
-import path from 'path';
+import RotatingFileStream from 'bunyan-rotating-file-stream';
+import fs from 'fs';
+
+initializeLogFile();
 
 const level = (process.env.NODE_LOGGING_LEVEL || 'info') as LogLevel;
 
@@ -11,10 +14,36 @@ const log = bunyan.createLogger({
       stream: process.stdout,
     },
     {
-      level,
-      path: path.resolve(__dirname, '../logs/logs.log'),
+      stream: new RotatingFileStream({
+        path: '/app/dist/logs/test.log',
+        period: '1d',
+        totalFiles: 10,
+        rotateExisting: true,
+        threshold: '10m',
+        totalSize: '20m',
+        gzip: true,
+      }),
     },
   ],
 });
+
+function initializeLogFile() {
+  // Heroku doesn't provide an opportunity to store logs in /var/log/, by this
+  // reason was decided to put logs into dist/logs
+
+  const dir = '/app/dist/logs/';
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.appendFile('/app/dist/logs/test.log', '', (error) => {
+    if (error) {
+      throw error;
+    }
+
+    console.log('Log file is initialized');
+  });
+}
 
 export { log };
