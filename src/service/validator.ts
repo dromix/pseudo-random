e4247f8709f4829020d93336dedde86b8476666a
Serhii <kalyuzhni.sergei@gmail.com>
2022-02-13 20:41:22 +0200

    Add new probability validation

--- src/service/validator.ts
+++ src/service/validator.ts
@@ -7,7 +7,7 @@ export class ValidatorService {
       check('probability')
         .custom((val) => typeof val === 'number' && val >= 0 && val <= 1)
         .withMessage('Probability should be a number between 0 and 1'),
-
+      check('probability').notEmpty(),
       check('value').notEmpty().withMessage('Value is required'),
       check('value')
         .custom((val) => typeof val === 'boolean')
